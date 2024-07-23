29feb0bc774c3fce31f34bb7a3f08bf102789aa9
Michael Afari <michealnewtons419@gmail.com>
2024-07-24 00:50:57 +0200

    Ng mocks

---
+++ src/app/models/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    id: number;
+    name: string;
+    price: number;
+    image_url: string;
+    constructor() {
+        this.id = 0;
+        this.name = "";
+        this.price = 0;
+        this.image_url = "";
+    }
+}
