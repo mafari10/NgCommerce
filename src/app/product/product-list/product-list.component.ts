7213d7f78441ef3e4ceb41f32394932054058e0d
Michael Afari <michealnewtons419@gmail.com>
2024-07-24 03:18:57 +0200

    Added some components and styling

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../models/product';
+import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CurrencyPipe, FlexLayoutModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  private productService = inject(ProductService);
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => this.products = products)
+  }
 
 }
